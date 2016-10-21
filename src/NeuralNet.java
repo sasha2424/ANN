a2a47e5d368c5d4ec475bbd3f8850b34a0e5d1ec
Alexander Ivanov <sashaivanov2424@gmail.com>
2016-10-21 08:23:11 -0700

    finished constructor

--- src/NeuralNet.java
+++ src/NeuralNet.java
@@ -8,7 +8,16 @@ public class NeuralNet {
 	
 	
 	public NeuralNet(int[] neurons){
+		//sets matrix array size
 		net = new Matrix[neurons.length];
+		
+		//sets input Matrix
+		net[0] = new Matrix(1,neurons[0]);
+		
+		//sets other Matrices 
+		for(int i = 1; i < net.length;i++){
+			net[i] = new Matrix(neurons[i-1],neurons[i]);
+		}
 	}
 	
 }
